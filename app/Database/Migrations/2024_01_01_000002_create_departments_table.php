992ea0795e27ba3e7b72686a0d017d84f4828db4
jerica08 <jericamarquez88@gmail.com>
2026-03-02 00:59:29 +0800

    Created database tables for users, departments, tickets, and ticket assignments

---
+++ app/Database/Migrations/2024_01_01_000002_create_departments_table.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CreateDepartmentsTable extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id' => [
+                'type'           => 'INT',
+                'constraint'     => 11,
+                'unsigned'       => true,
+                'auto_increment' => true,
+            ],
+            'name' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '255',
+                'null'       => false,
+            ],
+            'created_at' => [
+                'type' => 'DATETIME',
+                'null' => false,
+            ],
+            'updated_at' => [
+                'type' => 'DATETIME',
+                'null' => false,
+            ],
+        ]);
+        $this->forge->addKey('id', true);
+        $this->forge->createTable('departments');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('departments');
+    }
+}
