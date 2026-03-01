e76763c5839e399d5552d8c373134bd9cc159911
jerica08 <jericamarquez88@gmail.com>
2026-03-02 01:46:04 +0800

    Created departments migration file for database

---
+++ app/Database/Migrations/2026-03-01-174540_CreateDepartmentsTable.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CreateDepartmentsTable extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id' => [
+                'type'           => 'INT',
+                'constraint'     => 11,
+                'unsigned'       => true,
+                'auto_increment' => true,
+            ],
+            'name' => [
+                'type'       => 'VARCHAR',
+                'constraint' => 150,
+            ],
+            'created_at DATETIME default current_timestamp',
+            'updated_at DATETIME default current_timestamp on update current_timestamp'
+        ]);
+
+        $this->forge->addKey('id', true);
+        $this->forge->createTable('departments');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('departments');
+    }
+}
